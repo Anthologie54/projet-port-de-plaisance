d803f3c42286a334467d82983c8985150eeac3da
Anthologie54 <alexandremechineau@gmail.com>
2025-07-09 13:15:05 +0200

    END models

---
+++ models/Catway.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const CatwaySchema = new Schema({
+    catwayNumber: {
+        type      : Number,
+        unique    : true,
+        required  : [true, 'Le numéro du Catway est requis']
+    },
+    catwayType:{
+        type      : String,
+        enum      : ['long', 'short'],
+        required  : [true, 'Le type du catway est requis']
+    },
+    catwayState:{
+        type      : String,
+        required  : [true, 'L\'état du catway est requis']
+    }
+});
+
+module.exports = mongoose.model ('Catway', CatwaySchema);
---
+++ models/Reservation.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const ReservationSchema = new Schema({
+    catwayNumber: {
+        type     : Number,
+        required : [true, 'Le numéro du catway est nécessaire.']
+    },
+    clientName: {
+        type     : String,
+        required : [true, 'Le nom du client est nécessaire.']
+    },
+    boatName: {
+        type     : String,
+        required : [true, 'Le nom du bateau est nécessaire.']
+    },
+    startDate: {
+        type     : Date,
+        required : [true, 'La date d\'arrivé est nécessaire.']
+    },
+    endDate:{
+        type     : Date,
+        required : [true, 'La date de départ est nécessaire.']
+    }
+});
+
+module.exports = mongoose.model ('Reservation', ReservationSchema);
